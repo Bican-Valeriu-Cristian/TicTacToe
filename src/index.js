c7e1f8fde9a054e2c78e869542284cfa84cc8974
Bican-Valeriu-Cristian <126001331+Bican-Valeriu-Cristian@users.noreply.github.com>
2025-04-09 17:01:53 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import App from './app.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
